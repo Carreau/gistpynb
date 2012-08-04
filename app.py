698166b3da0afacda3bca9d720585dc1e9ab8130
Bussonnier Matthias <bussonniermatthias@gmail.com>
2012-08-04 15:05:52 +0200

    add flat pages

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask , request
 import nbconvert.nbconvert as nbconvert
 import requests
 from nbformat import current as nbformat
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/greate', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below this is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
 
 
 @app.route('/url/<path:url>')
